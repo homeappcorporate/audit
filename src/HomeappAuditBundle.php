58aa796b8df097470a23fdd84ea4bc6145d57733
Legion112 <ageev.maks2014@gmail.com>
2021-06-03 12:07:10 +0300

    Created bundle

--- src/HomeappAuditBundle.php
+++ src/HomeappAuditBundle.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Homeapp\AuditBundle;
 
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
 /** @psalm-suppress UnusedClass */
-final class HomeappAuditBundle
+final class HomeappAuditBundle extends Bundle
 {
     public function save(): string
     {
